f831e0b98bed3fa7fbc0c085ac31f2975cc39d63
ext.cuixuecheng1 <ext.cuixuecheng1@jd.com>
2026-06-16 11:02:51 +0800

    Initial Commit

---
+++ readBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  readBook
+//
+//  Created by ext.cuixuecheng1 on 2026/6/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ readBook/readBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  readBookApp.swift
+//  readBook
+//
+//  Created by ext.cuixuecheng1 on 2026/6/16.
+//
+
+import SwiftUI
+
+@main
+struct readBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
